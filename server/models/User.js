137f4c1b1f1daafe7e791cf9597c62ec30c3cbcd
ShreyasPathe <patheshreyas7@gmail.com>
2025-07-10 22:57:01 +0530

    clerk auth issue resolved

--- server/models/User.js
+++ server/models/User.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     _id: { type: String, required: true },
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    imageUrl: { type: String, required: true },
+    name: { type: String }, // not required
+    email: { type: String }, // not required
+    imageUrl: { type: String }, // not required
     enrolledCourses: [
         {
             type: mongoose.Schema.Types.ObjectId,
