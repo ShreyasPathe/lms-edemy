4027075b56e78f21c35714de32f574e1d75da2b1
ShreyasPathe <patheshreyas7@gmail.com>
2025-07-16 00:47:41 +0530

    design changes

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 import { AppContext } from '../../context/AppContext';
 import { UserButton, useUser } from '@clerk/clerk-react';
 
-const Navbar = ({ bgColor }) => {
+const Navbar = () => {
 
   const { isEducator } = useContext(AppContext)
   const { user } = useUser()
 
   return isEducator && user && (
-    <div className={`flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3 ${bgColor}`}>
+    <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-200 py-3 bg-white">
       <Link to="/">
         <img src={assets.logo} alt="Logo" className="w-28 lg:w-32" />
       </Link>
-      <div className="flex items-center gap-5 text-gray-500 relative">
+      <div className="flex items-center gap-5 text-gray-800 relative">
         <p>Hi! {user.fullName}</p>
         <UserButton />
       </div>
